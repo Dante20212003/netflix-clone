6a3984630074bc568973e6ca16e5039f72bf34d4
Dante <danteariastarifa@gmail.com>
2022-10-08 15:29:34 -0400

    modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NetflixApp from "./NetflixApp";
+import { Provider } from "react-redux";
 import "./styles.css";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <NetflixApp />
+    <Provider store={store}>
+      <NetflixApp />
+    </Provider>
   </React.StrictMode>
 );
