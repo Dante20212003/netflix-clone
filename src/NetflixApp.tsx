5e724c03787fa0436213a61b90821a4199b031e6
Dante <danteariastarifa@gmail.com>
2022-10-08 15:33:05 -0400

    Add Scroll on visit new page

--- src/NetflixApp.tsx
+++ src/NetflixApp.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "@/routes/AppRouter";
+import { ScrollToTop } from "./components";
 
 const NetflixApp = () => {
   return (
     <BrowserRouter>
       <AppRouter />
+      <ScrollToTop />
     </BrowserRouter>
   );
 };
